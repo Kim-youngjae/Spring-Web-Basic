f4f83b7820a849ed614956d2bddad23979a92185
Kim-youngjae <youngjae459@gmail.com>
2023-12-04 16:10:10 +0900

    init

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,22 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data // 핵심 도메인은
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price; // 가격이 입력 안되는 경우를 위해 Integer 사용 -> null 허용
+    private Integer quantity; // 가격과 사용 이유 동일
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
